baa6fe4fe01540f1549c201a3d60f2d716537a50
Alumno <Alumno@b405-01>
2025-08-12 16:18:57 -0400

    Agregar archivos de proyecto.

---
+++ AppBlazor.Entities/LibroFormCLS.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppBlazor.Entities
+{
+    public class LibroFormCLS
+    {
+        [Required (ErrorMessage="El id es requerido")]
+        [Range(1, int.MaxValue, ErrorMessage ="El valor debe ser un numero positivo")]
+        public int idLibro { get; set; }
+        [Required (ErrorMessage = "El titulo es requerido")]
+        [MaxLength (100, ErrorMessage = "La longitud maxima es 100 caracteres")]
+        public string titulo { get; set; } = string.Empty;
+        [Required (ErrorMessage = "El resumen es requerido")]
+        [MinLength(20, ErrorMessage = "La longitud minima es 20 caracteres")]
+
+        public string resumen { get; set; } = null!;
+
+    }
+}
---
+++ AppBlazor.test/LibroFormCLSTest.cs
@@ -0,0 +1,39 @@
+using AppBlazor.Entities;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+namespace AppBlazor.test
+{
+    public class LibroFormCLSTest
+    {
+        private List<ValidationResult> ValidaModelo(object modelo)
+        {
+            var resultados=new List<ValidationResult>();
+            var contexto = new ValidationContext(modelo,null,null);
+            Validator.TryValidateObject(modelo, contexto, resultados, true);
+            return resultados;
+        }
+        [Fact]
+        public void ValidacionCamposVacios()
+        {
+            var libro=new LibroFormCLS();
+            var errores = ValidaModelo(libro);
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El id es requerido") ||
+                e.ErrorMessage!.Contains("El valor debe ser un numero positivo"));
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El titulo es requerido"));
+            Assert.Contains(errores, e => e.ErrorMessage!.Contains("El resumen es requerido"));
+        }
+        [Fact]
+        public void ValidacionCamposCorrectos()
+        {
+            var libro = new LibroFormCLS
+            {
+                idLibro = 1,
+                titulo = "Libro de Prueba",
+                resumen = "Es un resumen del libro de prueba."
+            };
+            var errores=ValidaModelo(libro);
+            Assert.Empty(errores);
+
+        }
+    }
+}
